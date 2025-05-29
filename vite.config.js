9c8ceadb99644fb6ae25d636fcb85db4706177e9
AlexSanLo <san.lope.alex@gmail.com>
2025-05-29 13:48:55 +0200

    Config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/ProyectoFinal-DAW/', 
+  base: '/ProyectoFinal-DAW-V2/', 
 })
