3a69e88696e0e66aa8d164906dea5cceee091934
AlexSanLo <san.lope.alex@gmail.com>
2025-05-29 16:49:27 +0200

    Updates

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,27 +1,19 @@
 // firebase-messaging-sw.js
-
-importScripts('https://www.gstatic.com/firebasejs/9.6.1/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/9.6.1/firebase-messaging-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.6.10/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.6.10/firebase-messaging-compat.js');
 
 firebase.initializeApp({
   apiKey: "AIzaSyA8EmogduR2W8d9ht3q4VyM4J-JUTvwBd8",
   authDomain: "noteeasy-7080d.firebaseapp.com",
   projectId: "noteeasy-7080d",
-  storageBucket: "noteeasy-7080d.appspot.com",
+  storageBucket: "noteeasy-7080d.firebaseapp.com",
   messagingSenderId: "982678504604",
   appId: "1:982678504604:web:efbc601021106951319959",
-  measurementId: "G-4FHPK9T92Y",
+  measurementId: "G-4FHPK9T92Y"
 });
 
 const messaging = firebase.messaging();
 
-messaging.onBackgroundMessage((payload) => {
-  console.log('[firebase-messaging-sw.js] Recibido mensaje en segundo plano: ', payload);
-  const notificationTitle = payload.notification.title;
-  const notificationOptions = {
-    body: payload.notification.body,
-    icon: '/path/to/icon.png'
-  };
-
-  self.registration.showNotification(notificationTitle, notificationOptions);
+messaging.onBackgroundMessage(function(payload) {
+  console.log("Mensaje recibido en segundo plano:", payload);
 });
