a018a748006d5d1e96c4058c109a874712455d77
AlexSanLo <san.lope.alex@gmail.com>
2025-05-29 17:01:27 +0200

    Prueba arreglo movil

--- src/main.js
+++ src/main.js
@@ -5,12 +5,23 @@ import { messaging } from "./firebase";
 
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('/ProyectoFinal-DAW-V2/firebase-messaging-sw.js')
-    .then((registration) => {
-      console.log('Service Worker registrado:', registration);
+    .then(registration => {
+      messaging.useServiceWorker(registration);
+      Notification.requestPermission()
+        .then(permission => {
+          if (permission === 'granted') {
+            console.log('Permiso concedido para mostrar notificaciones');
+          } else {
+            console.log('Permiso denegado para mostrar notificaciones');
+          }
+        });
     })
-    .catch((error) => {
-      console.error('Error al registrar el Service Worker:', error);
+    .catch(error => {
+      console.error('Error al registrar el servicio worker:', error);
     });
+} else {
+  console.log('Service worker no soportado en este navegador');
 }
 
 createApp(App).mount('#app');
+
