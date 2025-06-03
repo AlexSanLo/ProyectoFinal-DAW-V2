752ea7ecdab1c9f065b61a120e7dab5a30735c0c
AlexSanLo <san.lope.alex@gmail.com>
2025-06-03 13:45:11 +0200

    Token

--- src/composables/firebaseMessaging.js
+++ src/composables/firebaseMessaging.js
@@ -2,20 +2,23 @@ import { getToken } from "firebase/messaging";
 import { messaging } from "../firebase";
 
 export function obtenerTokenFCM() {
-  return getToken(messaging, { 
-    vapidKey: 'BBaCgc9snIa4Ge2qMfubxh-EP-uwSOYPSM-Aqq91uZCoLH2LHJoNO3XRB5cGET2QC335dQdQpKBFMPC4Wql-kNI'
-  })
-    .then((currentToken) => {
-      if (currentToken) {
-        console.log('¡Token FCM obtenido con éxito!', currentToken);
-        return currentToken;
-      } else {
-        console.log('No se pudo generar el token, solicita permisos de notificación.');
-        return null;
-      }
-    })
-    .catch((err) => {
-      console.error('Error al obtener el token FCM:', err);
-      return null;
+  return navigator.serviceWorker.ready.then((registration) => {
+    return getToken(messaging, { 
+      vapidKey: 'BBaCgc9snIa4Ge2qMfubxh-EP-uwSOYPSM-Aqq91uZCoLH2LHJoNO3XRB5cGET2QC335dQdQpKBFMPC4Wql-kNI',
+      serviceWorkerRegistration: registration
     });
+  })
+  .then((currentToken) => {
+    if (currentToken) {
+      console.log('¡Token FCM obtenido con éxito!', currentToken);
+      return currentToken;
+    } else {
+      console.log('No se pudo generar el token, solicita permisos de notificación.');
+      return null;
+    }
+  })
+  .catch((err) => {
+    console.error('Error al obtener el token FCM:', err);
+    return null;
+  });
 }
